2872f4422e7f8a044a20f979877e79e579f82ac3
dungcatcher <ewan.odenthal1@gmail.com>
2022-05-08 12:34:36 +1000

    Enemies break walls

--- enemies.py
+++ enemies.py
@@ -14,6 +14,7 @@ class Enemy(pygame.sprite.Sprite):
         self.image.fill((0, 0, 255))
         self.rect = self.image.get_rect(center=self.pixel_position)
         self.path = []
+        self.wall_obstructions = []
         self.path_searched = False
         self.vel = [0, 0]
         self.speed = 0.5
@@ -21,7 +22,7 @@ class Enemy(pygame.sprite.Sprite):
         self.health = 10
         self.worth = 10
         self.damage = 2
-        self.range = 5
+        self.range = 1.5
         self.original_cooldown = 30
         self.cooldown = self.original_cooldown
         self.closest_tower = None
@@ -30,7 +31,7 @@ class Enemy(pygame.sprite.Sprite):
         current_closest_point, current_closest_tower = None, None
         shortest_distance = 9999999
         for tower in tower_group:
-            if not tower.is_wall and not search_walls:
+            if (tower.is_wall and search_walls) or (not tower.is_wall and not search_walls):
                 for point in tower.positions_covered:
                     distance_to_point = math.hypot((self.position[0] - point[0]), (self.position[1] - point[1]))
                     if distance_to_point < shortest_distance:
@@ -50,6 +51,15 @@ class Enemy(pygame.sprite.Sprite):
             path = astar.run(self.position, closest_point)
             if path is not None:
                 return path
+            else:  # Path could not be found, break walls to get through
+                clear_map = [[0 for x in range(45)] for y in range(45)]  # No obstructions
+                clear_astar = Astar(clear_map)
+                clear_path = clear_astar.run(self.position, closest_point)
+                for point in clear_path:
+                    for tower in tower_group:
+                        if [tower.position[0], tower.position[1]] == point and tower.is_wall:
+                            self.wall_obstructions.append(tower)
+                return clear_path
         return []
 
     def attack(self, tower, game):
@@ -77,13 +87,26 @@ class Enemy(pygame.sprite.Sprite):
         else:
             self.vel = [0, 0]
 
+        #  Attack target tower when in range
         if self.closest_tower is not None:
             pixel_diff_vector = Vector2(self.closest_tower.rect.centerx - self.rect.centerx,
                                         self.closest_tower.rect.centery - self.rect.centery)
             pixel_distance_squared = pixel_diff_vector.length_squared()
-            print(pixel_distance_squared <= (self.range * self.range) * game.tile_size)
             if pixel_distance_squared <= (self.range * self.range) * (game.tile_size * game.tile_size):
                 self.attack(self.closest_tower, game)
+                self.vel = [0, 0]
+
+        #  Attack wall obstructions when in range
+        if self.wall_obstructions:
+            closest_wall, closest_point = self.get_closest_tower(self.wall_obstructions, search_walls=True)
+            pixel_diff_vector = Vector2(closest_wall.rect.centerx - self.rect.centerx,
+                                        closest_wall.rect.centery - self.rect.centery)
+            pixel_distance_squared = pixel_diff_vector.length_squared()
+            if pixel_distance_squared <= (self.range * self.range) * (game.tile_size * game.tile_size):
+                self.attack(closest_wall, game)
+                if closest_wall.health <= 0:
+                    self.wall_obstructions.remove(closest_wall)
+                self.vel = [0, 0]
 
         self.position = [int(self.pixel_position[0] // game.tile_size), int(self.pixel_position[1] // game.tile_size)]
         self.pixel_position[0] += self.vel[0] * self.speed
